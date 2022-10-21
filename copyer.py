b7443bc020206f36c54f47c139800b131641ca3a
jiping wang <jping1993@outlook.com>
2022-10-22 00:14:51 +0800

    test 添加节点方法 extend（）

--- copyer.py
+++ copyer.py
@@ -9,7 +9,7 @@
 
 """
 import os
-import lxml.etree as et
+import lxml.etree as lxmlET
 import pypinyin
 import xml.dom.minidom
 
@@ -100,45 +100,6 @@ class FilterFiles:
 # ff = FilterFiles()
 
 
-def pinyinized_xml(result_dict: dict, find_name: str, find_node: str, find_property: str):
-
-    for key in result_dict.keys():
-        # 根据filter_name 的值 来判断key 是否匹配 筛选出来所需要处理的xml
-        if find_name in key:
-            # match_list.append(result_dict.get(key))
-            # for xml_file in match_list:
-            xml_tree = et.parse(result_dict.get(key))
-
-            xml_root = xml_tree.getroot()
-
-            count = 0
-            for item in xml_root.findall(find_node):
-                v = item.get(find_property)
-                if bool(is_chinese(v)):
-                    py = get_pinyin(v)
-                    item.set(find_property, py)
-                    count = count + 1
-                    # print(item)
-            print(f" {count} changes from {key}")
-            xml_tree.write(result_dict.get(key), encoding="utf-8", xml_declaration=True)
-
-
-# def edit_xml(xml_file_list: list, find_node: str, chang_property: str):
-#     for xml_file in xml_file_list:
-#         xml_tree = et.parse(xml_file)
-#         xml_root = xml_tree.getroot()
-#         count = 0
-#         for item in xml_root.findall(find_node):
-#             v = item.get(chang_property)
-#             if bool(is_chinese(v)):
-#                 py = get_pinyin(v)
-#                 item.set(chang_property, py)
-#                 count = count + 1
-#                 # print(item)
-#         print(f" {count} changes from {xml_file} ")
-#         xml_tree.write(xml_file, encoding="utf-8", xml_declaration=True)
-
-
 # 判断是否包含中文
 def is_chinese(string):
     check_str = string
@@ -227,62 +188,46 @@ def create_icon_pack(file_list):
     print("ICON_PACK-XML File Exported")
 
 
-xml_file_dir = "/Users/wangjiping/PycharmProjects/copyer/IconRequest-20221001_122347"
-xml_file_dir2 = r"C:\Users\YOWH\PycharmProjects\copyer\IconRequest-20221001_122347"
-
-rdict = {"appfilter_12331": "string1", "avpfilter_2331": "strin3234g1", "appfilter_123431": "3424", }
-# read_xml(rdict, "filter")
-ff = FilterFiles()
-# rs_list = ff.filter_files(xml_file_dir2, prefix=["app", "theme"], suffix=[".xml"])
-# appmaplist = read_xml(rs_list, "appmap", "item", "name")
-# rename_xml(appmaplist,"item","name")
-
-png_dir = "/Users/WangChunsheng/PycharmProjects/copyer/IconRequest-20221001_122347"
-# png_list = ff.filter_suffix(suffix=[".png"], file_list=None,
-#                             file_dir="/Users/WangChunsheng/PycharmProjects/copyer/IconRequest-20221001_122347")
-# print(png_list)
-# rename_file(png_list, file_dir=png_dir)
-xml_dict = ff.filter_files(xml_file_dir, prefix=["appfilter", "appmap", "theme_resources"], suffix=[".xml"])
-print(xml_dict)
-pinyinized_xml(xml_dict, "appmap", "item", "name")
-
-# project_dir = input("输入包含替换资源的项目文件夹\n")
-# name = "appfilter"
-
-route = input('请输入要查找的路径：')
-name = input('请输入要查找的文件：')
-
-"""
-从 xml dict 里面 找 对应文件 比如 appmap 
-再 project xml dict 里面找 对应文件 appmap 文件
-
-"""
-
-
-# 判断文件夹名称 是否拷贝过 在目标文件注释里面
-# 第一步获取列表时候 保存一个变量 用于后续比对
-#
-# 读取请求文件夹路径中的xml 读取节点
-# 读取目标文件夹 xml 读取节点
-#         统计一下item条数
-#     固定条件读取和对应匹配
-#         appfilter
-#         drawable
-#         appmap
-#         theme_res
-#         icon_pack
-#         changlog item 图标适配个数
-#
-# 添加原节点到目标节点上
-# 添加注释信息 带有时间的文件夹名（其他必要信息）
-def move_xml_info(req_xml_dir: dict, project_dir: dict, png_list, req_name: str, ):
-    for key in req_xml_dir.keys():
+def pinyinized_xml(result_dict: dict, find_name: str, find_node: str, find_property: str):
+    for key in result_dict.keys():
         # 根据filter_name 的值 来判断key 是否匹配 筛选出来所需要处理的xml
-        if "appfilter" in key:
-            xml_tree = et.parse(req_xml_dir.get(key))
+        if find_name in key:
+            # match_list.append(result_dict.get(key))
+            # for xml_file in match_list:
+            xml_tree = lxmlET.parse(result_dict.get(key))
             xml_root = xml_tree.getroot()
-            item = xml_root.findall("item")
-            print(item)
+
+            count = 0
+            for item in xml_root.findall(find_node):
+                v = item.get(find_property)
+                if bool(is_chinese(v)):
+                    py = get_pinyin(v)
+                    item.set(find_property, py)
+                    count = count + 1
+                    # print(item)
+            print(f" {count} changes from {key}")
+            xml_tree.write(result_dict.get(key), encoding="utf-8", xml_declaration=True)
+
+
+#  project_dir: dict, png_list, req_name: str,
+def move_xml_info(req_xml_dir: dict, project_dir: dict, png_list, req_name: str):
+    for req_key in req_xml_dir.keys():
+        # 根据filter_name 的值 来判断key 是否匹配 筛选出来所需要处理的xml
+        if "appfilter" in req_key:
+            req_xml_tree = lxmlET.parse(req_xml_dir.get(req_key))
+            req_xml_root = req_xml_tree.getroot()
+            req_xml_root.remove(req_xml_root.find("iconback"))
+            req_xml_root.remove(req_xml_root.find("iconmask"))
+            req_xml_root.remove(req_xml_root.find("iconupon"))
+            req_xml_root.remove(req_xml_root.find("scale"))
+            for pro_key in project_dir:
+                if "appfilter" in pro_key:
+                    pro_xml_tree = lxmlET.parse(project_dir.get(pro_key))
+                    pro_xml_root = pro_xml_tree.getroot()
+                    pro_xml_root.extend(req_xml_root.findall("*"))
+
+            req_xml_tree.write(req_xml_dir.get(key), encoding="utf-8", xml_declaration=True)
+            print("ok")
             count = 0
 
     print()
@@ -311,3 +256,56 @@ def find_file(project_dir: str, find_name: str):
 #   - 先 解析 目标文件
 #   - 增加 节点数据
 #   - 更新 保存文件
+
+
+xml_file_dir = "/Users/wangjiping/PycharmProjects/copyer/IconRequest-20221001_122347"
+xml_file_dir3 = "/Users/WangChunsheng/PycharmProjects/copyer/IconRequest-20221001_122347"
+xml_file_dir2 = r"C:\Users\YOWH\PycharmProjects\copyer\IconRequest-20221001_122347"
+
+rdict = {"appfilter_12331": "string1", "avpfilter_2331": "strin3234g1", "appfilter_123431": "3424", }
+# read_xml(rdict, "filter")
+ff = FilterFiles()
+# rs_list = ff.filter_files(xml_file_dir2, prefix=["app", "theme"], suffix=[".xml"])
+# appmaplist = read_xml(rs_list, "appmap", "item", "name")
+# rename_xml(appmaplist,"item","name")
+
+png_dir = "/Users/WangChunsheng/PycharmProjects/copyer/IconRequest-20221001_122347"
+# png_list = ff.filter_suffix(suffix=[".png"], file_list=None,
+#                             file_dir="/Users/WangChunsheng/PycharmProjects/copyer/IconRequest-20221001_122347")
+# print(png_list)
+# rename_file(png_list, file_dir=png_dir)
+xml_dict = ff.filter_files(xml_file_dir3, prefix=["appfilter", "appmap", "theme_resources"], suffix=[".xml"])
+print(xml_dict)
+pinyinized_xml(xml_dict, "theme", "AppIcon", "image")
+
+# project_dir = input("输入包含替换资源的项目文件夹\n")
+# name = "appfilter"
+move_xml_info(xml_dict)
+
+# route = input('请输入要查找的路径：')
+# name = input('请输入要查找的文件：')
+
+"""
+从 xml dict 里面 找 对应文件 比如 appmap 
+再 project xml dict 里面找 对应文件 appmap 文件
+
+"""
+
+
+# 判断文件夹名称 是否拷贝过 在目标文件注释里面
+# 第一步获取列表时候 保存一个变量 用于后续比对
+#
+# 读取请求文件夹路径中的xml 读取节点
+# 读取目标文件夹 xml 读取节点
+#         统计一下item条数
+#     固定条件读取和对应匹配
+#         appfilter
+#         drawable
+#         appmap
+#         theme_res
+#         icon_pack
+#         changlog item 图标适配个数
+#
+# 添加原节点到目标节点上
+# 添加注释信息 带有时间的文件夹名（其他必要信息）
+#  project_dir: dict, png_list, req_name: str,
--- test.py
+++ test.py
@@ -1,8 +1,36 @@
+# import xml.etree.ElementTree as ET
+import lxml.etree as ET
 
-import lxml.etree as et
+file = "/Users/WangChunsheng/PycharmProjects/copyer/IconRequest-20221001_122347/appmap_20220922_115417.xml"
+file2 = "/Users/WangChunsheng/PycharmProjects/copyer/IconRequest-20221001_122347/appmap_20221001_122343.xml"
 
-file = "/Users/wangjiping/PycharmProjects/copyer/IconRequest-20221001_122347/appfilter_20221001_122347.xml"
-xml_tree = et.parse(file)
+xml_tree = ET.parse(file)
 xml_root = xml_tree.getroot()
-xml_root.
-print(xml_root)
+# print(xml_root.findall("."))
+
+xml_tree2 = ET.parse(file2)
+xml_root2 = xml_tree2.getroot()
+# xml_root2.extend(xml_root.iter("appmap"))
+pos_num = -1
+for item in xml_root.iter("appmap"):
+    # print(item)
+    # if item != "appmap":
+    #     print(item)
+    # if item == "appmap":
+    #     print("appmap")
+    #     print(bool(item.tag == "appmap"))
+    #     # pass
+    # else:
+    #     print(item)
+    #     print(bool(item.tag == "appmap"))
+    xml_root2.extend(item)
+
+# xml_root2.extend(xml_root.findall("{*}*"))
+# for item in xml_root.iter("*"):
+#
+#     xml_root2.insert(0, item)
+
+# xml_root2.remove(xml_root2.find("appmap"))
+
+xml_tree2.write(file2, encoding="utf-8", xml_declaration=True)
+# print(xml_root2.findall("*"))
